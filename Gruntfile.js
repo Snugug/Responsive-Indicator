d5a2358a39fae027c64ae2f914ee0c2a0e032328
Sam Richard <snugug@gmail.com>
2013-08-29 08:50:20 -0400

    Gruntfile for Hinting/Building

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+(function () {
+  'use strict';
+
+  module.exports = function (grunt) {
+
+    grunt.initConfig({
+      watch: {
+        js: {
+          files: ['build/{,**/}*.js'],
+          tasks: ['jshint']
+        }
+      },
+
+      jshint: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        all: ['build/{,**/}*.js']
+      },
+
+      uglify: {
+        dist: {
+          options: {
+            mangle: true,
+            compress: true
+          },
+          files: [{
+            expand: true,
+            cwd: 'build',
+            src: ['**/*.js'],
+            dest: 'dist',
+            ext: '.min.js'
+          }]
+        }
+      },
+
+      bump: {
+        options: {
+          files: [
+            'package.json',
+            'bower.json'
+          ],
+          commit: true,
+          commitMessage: 'Release v%VERSION%',
+          commitFiles: ['-a'],
+          createTag: true,
+          tagName: 'v%VERSION%',
+          tagMessage: 'Version %VERSION%',
+          push: true,
+          pushTo: 'origin'
+        }
+      }
+    });
+
+    //////////////////////////////
+    // Grunt Task Loads
+    //////////////////////////////
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    //////////////////////////////
+    // Build
+    //////////////////////////////
+    grunt.registerTask('build', 'Builds distribution code', function() {
+      grunt.task.run('jshint', 'uglify');
+    });
+  };
+}());
